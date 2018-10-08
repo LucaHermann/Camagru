06f9d13caa37d52dcfc4dab5791344a08a72be24
Luca Hermann <luca@devnullMacbook.local>
2018-10-08 15:14:15 +0200

    [lhermann] - add config/database.php and work on connect.php

--- php/profile.php
+++ php/profile.php
@@ -5,7 +5,7 @@ session_start();
 $idasked = $_GET['id'];
 
 $reponse = $bdd->prepare('SELECT * FROM user WHERE id = '.$idasked.'');
-$reponse = $bdd->bindValue('id', $idasked, PDO::PARAM_INT);
+//$reponse = $bdd->bindValue('id', $idasked, PDO::PARAM_INT);
 $reponse->execute();
 while ($data = $reponse->fetch())
 {
