2da8fffa4cf6a9616f44363aff3ec16645942b41
luca <lhermann@student.42.fr>
2018-10-19 14:37:06 +0200

    do test on comment.php || resolve comment scrool problems

--- js/comment.js
+++ js/comment.js
@@ -1,27 +1,27 @@
 function comment_send(form){
-    var image =  form.elements[1].value;
-    var donnee = form.elements[0].value;
-    document.getElementById('comment_index'+image).value = "";
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState == 4 && xhr.status == 200){    
-            comment(image);
-        }
-    }
-    xhr.open('POST','./comment.php',true);
-    xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-    xhr.send("idimg="+image+'&'+"text="+donnee);
+	var image =  form.elements[1].value;
+	var donnee = form.elements[0].value;
+	document.getElementById('comment_index'+image).value = "";
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+			if(xhr.readyState == 4 && xhr.status == 200){    
+					comment(image);
+			}
+	}
+	xhr.open('POST','./comment.php',true);
+	xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+	xhr.send("idimg="+image+'&'+"text="+donnee);
 }
 
 function comment(image){
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState == 4 && xhr.status == 200){
-            leselect = xhr.responseText;
-            document.getElementById('comment'+image).innerHTML = leselect;
-        }
-    }
-    xhr.open("POST","aff_index_comment.php",true);
-    xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-    xhr.send("idimg="+image);
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+			if(xhr.readyState == 4 && xhr.status == 200){
+					leselect = xhr.responseText;
+					document.getElementById('comment'+image).innerHTML = leselect;
+			}
+	}
+	xhr.open("POST","aff_index_comment.php",true);
+	xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+	xhr.send("idimg="+image);
 }
