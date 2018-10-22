729cdcc2ea43ba3c0e43607d922930527951d36f
Maxime DAUPHINOT <mdauphin@e1r11p3.42.fr>
2018-10-22 17:05:03 +0200

    [mdauphin] - index_delog

---
+++ js/comment_delog.js
@@ -0,0 +1,16 @@
+function comment_send(form){
+    alert("Vous devez etre connecté pour commenter une photo!");
+}
+
+function comment(image){
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+			if(xhr.readyState == 4 && xhr.status == 200){
+					leselect = xhr.responseText;
+					document.getElementById('comment'+image).innerHTML = leselect;
+			}
+	}
+	xhr.open("POST","aff_index_comment.php",true);
+	xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+	xhr.send("idimg="+image);
+}
