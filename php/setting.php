4fd7c33f41237b167011d08e85c0df7a9f798cd0
luca <lhermann@student.42.fr>
2018-10-22 18:05:28 +0200

    Add php/setting_validation.php

--- php/setting.php
+++ php/setting.php
@@ -38,21 +38,21 @@
 			<form class="setting_form">
 				<div class="input_zone">
 					<p>Change Username :<p><input type="text" class="formulary_name" placeholder="New Username">
-					 <div class="position_button">
-                        <button class="button">Submit</button>
-                    </div>
+					<div class="position_button">
+          	<button class="button">Submit</button>
+          </div>
 				</div>
 				<div class="input_zone_sd">
 					<p>Change Password :<p><input type="password" class="formulary_name" placeholder="New Password">
-					 <div class="position_button">
-                        <button class="button">Submit</button>
-                    </div>
+					<div class="position_button">
+          	<button class="button">Submit</button>
+          </div>
 				</div>
 				<div class="input_zone_td">
 					<p>Change Mail :<p><input type="text" class="formulary_name" placeholder="New Mail">
-					 <div class="position_button_td">
-                        <button class="button">Submit</button>
-                    </div>
+					<div class="position_button_td">
+          	<button class="button">Submit</button>
+          </div>
 				</div>
 			</form>
 		</div>
