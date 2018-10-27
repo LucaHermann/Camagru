155a933c0692b816a66756e05c033a96ed0433a2
luca <lhermann@student.42.fr>
2018-10-27 19:24:11 +0200

    create new_password/new_password_validation .php/.css

--- php/new_password.php
+++ php/new_password.php
@@ -6,27 +6,27 @@
 </head>
 <body>
 <div id="container">
-  <div id="formulary">
-      <div id="formulary_title">
-        <img class="logo" src="../ressources/logo_name.png">
-      </div>
-      <div id="formulary_form">
-          <div id="formulary_form_advertisement">
-            <h2>Reset password</h2>
-            <p class="advertisement">Reset your password just here</p>
-        </div>
-        <form method="post" action="new_password_validation.php" class="form_design">
-            <input type="password" id="formulary_new_password" placeholder="New Password">
-        </form>
-            <div class="position_button">
-                <button class="button">Reset password</button>
-            </div>
-    </div>
+	<div id="formulary">
+			<div id="formulary_title">
+				<img class="logo" src="../ressources/logo_name.png">
+			</div>
+			<div id="formulary_form">
+					<div id="formulary_form_advertisement">
+						<h2>Reset password</h2>
+						<p class="advertisement">Reset your password just here</p>
+				</div>
+				<form method="post" action="new_password_validation.php" class="form_design">
+						<input type="password" id="formulary_new_password" placeholder="New Password">
+				</form>
+						<div class="position_button">
+								<button class="button">Reset password</button>
+						</div>
+		</div>
 </div>
 <div id="footer">
-  <div id="footer_bar">
-    <strong> © Mdauphin Lhermann </strong>
-  </div>
+	<div id="footer_bar">
+		<strong> © Mdauphin Lhermann </strong>
+	</div>
 </div>
 </body>
 </html>
