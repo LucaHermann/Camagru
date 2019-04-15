0e2a36de22b18b1b05a1659517718135028e034c
luca <lhermann@student.42.fr>
2019-04-15 11:27:45 +0200

    [lhermann] - sql error modified is ok

--- sql/camagru.sql
+++ sql/camagru.sql
@@ -7,8 +7,10 @@
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone
+= "+00:00";
 
 --
 -- Database: `camagru`
@@ -20,12 +22,17 @@ SET time_zone = "+00:00";
 -- Table structure for table `comment`
 --
 
-CREATE TABLE `comment` (
-  `user_id` int(11) NOT NULL,
-  `img_id` int(11) NOT NULL,
-  `text` varchar(140) NOT NULL,
+CREATE TABLE `comment`
+(
+  `user_id` int
+(11) NOT NULL,
+  `img_id` int
+(11) NOT NULL,
+  `text` varchar
+(140) NOT NULL,
   `date` date NOT NULL,
-  `idcomment` int(100) NOT NULL
+  `idcomment` int
+(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -34,12 +41,17 @@ CREATE TABLE `comment` (
 -- Table structure for table `image`
 --
 
-CREATE TABLE `image` (
+CREATE TABLE `image`
+(
   `date` date NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `idimg` int(11) NOT NULL,
-  `img_path` varchar(1000) NOT NULL,
-  `filter_path` varchar(200) DEFAULT NULL
+  `id_user` int
+(11) NOT NULL,
+  `idimg` int
+(11) NOT NULL,
+  `img_path` varchar
+(1000) NOT NULL,
+  `filter_path` varchar
+(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -48,10 +60,14 @@ CREATE TABLE `image` (
 -- Table structure for table `likes`
 --
 
-CREATE TABLE `likes` (
-  `id_image` int(11) NOT NULL,
-  `id_utilisateur` int(11) NOT NULL,
-  `idlike` int(100) NOT NULL
+CREATE TABLE `likes`
+(
+  `id_image` int
+(11) NOT NULL,
+  `id_utilisateur` int
+(11) NOT NULL,
+  `idlike` int
+(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -60,14 +76,21 @@ CREATE TABLE `likes` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `fullname` varchar(50) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `password` varchar(1000) NOT NULL,
+CREATE TABLE `user`
+(
+  `id` int
+(11) NOT NULL,
+  `email` varchar
+(50) NOT NULL,
+  `fullname` varchar
+(50) NOT NULL,
+  `username` varchar
+(30) NOT NULL,
+  `password` varchar
+(1000) NOT NULL,
   `img` longblob NOT NULL,
-  `codepw` int(6) NOT NULL
+  `codepw` int
+(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -78,30 +101,39 @@ CREATE TABLE `user` (
 -- Indexes for table `comment`
 --
 ALTER TABLE `comment`
-  ADD PRIMARY KEY (`idcomment`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `img_id` (`img_id`);
+ADD PRIMARY KEY
+(`idcomment`),
+ADD KEY `user_id`
+(`user_id`),
+ADD KEY `img_id`
+(`img_id`);
 
 --
 -- Indexes for table `image`
 --
 ALTER TABLE `image`
-  ADD PRIMARY KEY (`idimg`),
-  ADD KEY `id_user` (`id_user`);
+ADD PRIMARY KEY
+(`idimg`),
+ADD KEY `id_user`
+(`id_user`);
 
 --
 -- Indexes for table `likes`
 --
 ALTER TABLE `likes`
-  ADD PRIMARY KEY (`idlike`),
-  ADD KEY `id_image` (`id_image`),
-  ADD KEY `id_utilisateur` (`id_utilisateur`);
+ADD PRIMARY KEY
+(`idlike`),
+ADD KEY `id_image`
+(`id_image`),
+ADD KEY `id_utilisateur`
+(`id_utilisateur`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY
+(`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -111,25 +143,29 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `idcomment` int(100) NOT NULL AUTO_INCREMENT,
+  MODIFY `idcomment` int
+(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `image`
 --
 ALTER TABLE `image`
-  MODIFY `idimg` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idimg` int
+(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `idlike` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `idlike` int
+(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int
+(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -139,18 +175,28 @@ ALTER TABLE `user`
 -- Constraints for table `comment`
 --
 ALTER TABLE `comment`
-  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`img_id`) REFERENCES `image` (`idimg`);
+ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY
+(`user_id`) REFERENCES `user`
+(`id`),
+ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY
+(`img_id`) REFERENCES `image`
+(`idimg`);
 
 --
 -- Constraints for table `image`
 --
 ALTER TABLE `image`
-  ADD CONSTRAINT `image_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+ADD CONSTRAINT `image_ibfk_1` FOREIGN KEY
+(`id_user`) REFERENCES `user`
+(`id`);
 
 --
 -- Constraints for table `likes`
 --
 ALTER TABLE `likes`
-  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`id_image`) REFERENCES `image` (`idimg`);
+ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY
+(`id_utilisateur`) REFERENCES `user`
+(`id`),
+ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY
+(`id_image`) REFERENCES `image`
+(`idimg`);
